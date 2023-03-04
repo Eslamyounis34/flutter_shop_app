7a3e025843b20d670aa77fc849facb917daf5449
Eslam-Youness <eslam-youness@elarabygroup.com>
2023-03-04 10:12:48 +0200

    add categories_model

---
+++ lib/models/categories_model.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CategoriesModel {
+  bool? status;
+  CategoriesDataModel? data;
+
+  CategoriesModel.fromjson(Map<String, dynamic> json) {
+    status = json['status'];
+    data = CategoriesDataModel.fromjson(json['data']);
+  }
+}
+
+class CategoriesDataModel {
+  int? current_page;
+  List<DataModel> data = [];
+
+  CategoriesDataModel.fromjson(Map<String, dynamic> json) {
+    current_page = json['current_page'];
+    json['data'].forEach((element) {
+      data.add(DataModel.fromjson(json['data']));
+    });
+  }
+}
+
+class DataModel {
+  int? id;
+  String? name;
+  String? image;
+
+  DataModel.fromjson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    image = json['image'];
+  }
+}
